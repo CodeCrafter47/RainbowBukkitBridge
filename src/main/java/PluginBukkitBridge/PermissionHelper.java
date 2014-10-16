7c79d34a14eabcfb766b747ab8ca8eadf639cf9c
Florian Stober <Flo.e3@gmx.de>
2014-10-16 18:03:27 +0200

    super perms, regex permissions

--- src/main/java/PluginBukkitBridge/PermissionHelper.java
+++ src/main/java/PluginBukkitBridge/PermissionHelper.java
@@ -1,33 +1,67 @@
 package PluginBukkitBridge;
 
-import java.util.concurrent.ConcurrentHashMap;
-
 import PluginReference.MC_Player;
+import org.bukkit.Bukkit;
+import org.bukkit.permissions.Permission;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PermissionHelper
 {
-	public static boolean hasPermission(MC_Player plr, String perm)
+    public static Map<Permission, List<Permission>> parents = new HashMap<>();
+
+	public static boolean hasPermission(MC_Player plr, String name)
 	{
-		// DBG
-		// -----------------------------------------------
-		String strName = "?";
-		if(plr != null) strName = plr.getName();
-		// -----------------------------------------------
-		
-		boolean res = false;
-		if(plr != null)
-		{
-			if(plr.isOp()) res = true;
-			else
-			{
-				res = MyPlugin.server.hasPermission(strName, perm);
-			}
-		}
-		
-		
-		//System.out.println(String.format("BukkitBridge -- Evaluating if '%s' has permission '%s': %s", strName, perm, "" + res));
-		// -----------------------------------------------
-		return res;
-		
+        boolean result = MyPlugin.server.hasPermission(plr.getName(), name);
+
+        if(result)return true;
+
+        Permission perm = Bukkit.getPluginManager().getPermission(name);
+
+        if(perm == null){
+            if(plr.isOp())return true;
+            // check regex
+            while ((name = getSuper(name)) != null){
+                if (MyPlugin.server.hasPermission(plr.getName(), name + ".*"))return true;
+            }
+
+        }else{
+            // check parents
+            for(Permission p: getParents(perm)){
+                if(MyPlugin.server.hasPermission(plr.getName(), p.getName()))return true;
+            }
+            // check regex
+            while ((name = getSuper(name)) != null){
+                if (MyPlugin.server.hasPermission(plr.getName(), name + ".*"))return true;
+            }
+        }
+
+        return false;
 	}
+
+    private static List<Permission> getParents(Permission perm){
+        if(parents.containsKey(perm))return parents.get(perm);
+
+        List <Permission> perms = new ArrayList<>();
+
+        for(Permission perm2 : Bukkit.getPluginManager().getPermissions()){
+            Map<String, Boolean> children = perm2.getChildren();
+            if(children.containsKey(perm.getName()) && children.get(perm.getName())){
+                perms.add(perm2);
+            }
+        }
+
+        parents.put(perm, perms);
+
+        return perms;
+    }
+
+    private static String getSuper(String s){
+        int end = s.lastIndexOf('.');
+        if(end <= 0)return null;
+        return s.substring(0, end);
+    }
 }
