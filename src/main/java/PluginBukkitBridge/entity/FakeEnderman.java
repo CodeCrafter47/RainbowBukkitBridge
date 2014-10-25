5dfb01c65b7034c368e5ad9817f8fa7041383036
Florian Stober <Flo.e3@gmx.de>
2014-10-25 15:50:49 +0200

    ENDERMAN

--- src/main/java/PluginBukkitBridge/entity/FakeEnderman.java
+++ src/main/java/PluginBukkitBridge/entity/FakeEnderman.java
@@ -1,7 +1,8 @@
 package PluginBukkitBridge.entity;
 
 import PluginBukkitBridge.MyPlugin;
-import PluginReference.MC_Entity;
+import PluginReference.MC_Enderman;
+import org.bukkit.Material;
 import org.bukkit.entity.Enderman;
 import org.bukkit.material.MaterialData;
 
@@ -9,14 +10,16 @@ import org.bukkit.material.MaterialData;
  * Created by florian on 12.10.14.
  */
 public class FakeEnderman extends FakeCreature implements Enderman{
-    public FakeEnderman(MC_Entity argEnt) {
+    MC_Enderman enderman;
+
+    public FakeEnderman(MC_Enderman argEnt) {
         super(argEnt);
+        enderman = argEnt;
     }
 
     @Override
     public MaterialData getCarriedMaterial() {
-        MyPlugin.fixme("stub method");
-        return null;
+        return Material.getMaterial(enderman.getCarriedBlock().getId()).getNewData((byte) enderman.getCarriedBlock().getSubtype());
     }
 
     @Override
