ed78365abb09431735fa5500521a3c8864290a6b
IsaiahPatton <isaiahpatton1@gmail.com>
2017-07-24 23:15:59 -0400

    implement Enderdragon isPhase & setPhase

--- src/main/java/PluginBukkitBridge/entity/FakeEnderdragon.java
+++ src/main/java/PluginBukkitBridge/entity/FakeEnderdragon.java
@@ -8,18 +8,20 @@ import org.bukkit.entity.EnderDragon;
  * Created by florian on 12.10.14.
  */
 public class FakeEnderdragon extends FakeComplexLivingEntity implements EnderDragon {
+    private Phase phase;
+    
     public FakeEnderdragon(MC_Entity argEnt) {
         super(argEnt);
+        this.phase = Phase.CIRCLING;
     }
 
     @Override
-    public Phase getPhase() {
-        MyPlugin.fixme();
-        return Phase.CIRCLING;
+    public Phase getPhase() {;
+        return this.phase;
     }
 
     @Override
     public void setPhase(Phase phase) {
-        MyPlugin.fixme();
+        this.phase = phase;
     }
 }
